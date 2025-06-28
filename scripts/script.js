ea1cd1472b6db897bbc7fec418e78211622db9a2
aquaseals <itzme.haya@gmail.com>
2025-06-27 22:03:02 -0400

    totaldisplacement done + kinematic equations wip

--- scripts/script.js
+++ scripts/script.js
@@ -36,31 +36,94 @@ function delta(a, b){
     return d
 }
 
-function checkStringForNums(string) {
+function checkStringForNums(string) { // func doesn't work
     return /\d/.test(string)
 }
+
+function radiansToDegrees(angle) {
+    return (angle * (180/Math.PI))
+}
+
+function degreesToRadians(angle) {
+    return (angle * (Math.PI /180))
+}
+
 
 function totalDisplacement(...args) {
     let displacements = args[0]
     let angles = args[1]
-    let x;
-    let y;
-    let atAngle;
+    let x = 0;
+    let y = 0;
 
     console.log(`displacements -> ${displacements}\nangles -> ${angles}`)
 
     for (let i=0; i<displacements.length; i++) {
-        //atAngle = checkStringForNums(angles[i])
-        atAngle = true
-        console.log(`is it at an angle? -> ${atAngle}`)
-        // if(atAngle) {
-        //     let angle = angles[i].split(" ")[1]
-        //     console.log(`angle number -> ${angle}`)
-        //     x += Math.sin(angle)*displacements[i]
-        //     y += Math.cos(angle)*displacements[i]
-        //     console.log(`total x -> ${x}\ntotal y -> ${y}`)
-        // }
+        if(angles[i].length > 1) {
+            let angle = angles[i].split(" ")[1]
+            console.log(`angle number -> ${angle}`)
+            let cos =  Math.cos(degreesToRadians(angle))
+            let sin =  Math.sin(degreesToRadians(angle))
+            x += sin*displacements[i]
+            y += cos*displacements[i]
+        } else if(angles[i].length === 1) {
+            let angle = angles[i]
+            let displacement = displacements[i]
+            switch (angle) {
+                case "N":
+                    y += displacement
+                    break
+                case "E":
+                    x += displacement
+                    break
+                case "S":
+                    y = y - displacement
+                    break
+                case "W":
+                    x = x - displacement
+                    break
+            }
+        }
+        console.log(`total x -> ${x}\ntotal y -> ${y}`)
     }
+    return {x, y}
+}
+
+function perTime(magnitude, direction, deltaTime) {
+    let averagePerTime = magnitude/deltaTime
+    if(direction.length === 0) {
+        return averagePerTime
+    } else {
+        return {averagePerTime, direction}
+    }
+}
+
+function kinematicEquation(d, t, vi, vf, a) {
+    let missing = ''
+    if (typeOf(d) === 'String') {
+        missing = 'd'
+    } else if (typeOf(t) === 'String') {
+        missing = 't'
+    } else if (typeOf(vi) === 'String') {
+        missing = 'vi'
+    } else if (typeOf(vf) === 'String') {
+        missing = 'vf'
+    } else if (typeOf(a) === 'String') {
+        missing = 'a'
+    }
+
+    switch (missing) {
+        case 'd':
+            break
+        case 't':
+            break
+        case 'vi':
+            break
+        case 'vf':
+            break
+        case 'a':
+            break
+    }
+
 }
 
 
